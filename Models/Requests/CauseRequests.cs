003c831c373bcd63b333d777f99646ae82d49bdd
Tion Blackmon <tion.blackmon@outlook.com>
2025-04-26 13:02:31 -0500

    Removed GetCauseRequest class as it is unnecessary when retrieving items (GET).

--- Models/Requests/CauseRequests.cs
+++ Models/Requests/CauseRequests.cs
@@ -1,12 +1,5 @@
 namespace VolunteerMatch.Models.Requests;
 
-public class GetCauseRequests
-{
-  public int Id { get; set; }
-  public string Name { get; set; }
-  public string ImageUrl { get; set; }
-  public string Description { get; set; }
-}
 public class CreateCauseRequest
 {
   public string Name { get; set; }
