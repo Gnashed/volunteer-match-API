2105757bda79d6f77c0e79689482d3abf0c3b6aa
Tion Blackmon <tion.blackmon@outlook.com>
2025-04-27 12:20:24 -0500

    Set <GenerateTargetFrameworkAttribute> to false in VolunteerMatch.Tests.csproj to prevent duplicate attributes for 'target framework'.

--- VolunteerMatch.Tests/UnitTest1.cs
+++ VolunteerMatch.Tests/UnitTest1.cs
@@ -8,11 +8,11 @@ public class CheckForDuplicateTests
     [Fact]
     public void Check_Duplicate_Name_ReturnTrue()
     {
-        // Arrange - set up data/ condition needed for testing.
+        // Arrange - set up data/condition needed for testing.
         var test = new CheckForDuplicate();
         
         // Act - Call the method
-        var result = test.IsDuplicate("Feeding America");
+        var result = test.IsDuplicate("Feeding Generations");
         
         // Assert - Verify the result
         Assert.True(result);
