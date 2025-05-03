769a6dff382c8f5a6e28518e90fda544024749d7
Tion Blackmon <tion.blackmon@outlook.com>
2025-05-03 14:34:15 -0500

    Added [Authorize] attribute to GET /api/volunteer endpoint (returns collection if bearer token is valid). Confirmed in Postman that I can retrieve the collection successfully.

--- VolunteerMatch/Controllers/VolunteerController.cs
+++ VolunteerMatch/Controllers/VolunteerController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VolunteerMatch.Data.Repositories.Interfaces;
 using VolunteerMatch.Models;
@@ -18,6 +19,7 @@ public class VolunteerController : ControllerBase
   }
 
   [HttpGet]
+  [Authorize]
   public async Task<IActionResult> GetAllAsyncTask()
   {
     var volunteers = await _volunteerRepository.GetAllAsync();
